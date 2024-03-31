e70d4101450ae6310fc9431f7731d3ac936bfdfc
DanilChebansky <151018836+DanilChebansky@users.noreply.github.com>
2024-03-31 03:44:57 +0300

    Added comments and repaired year-output in main.py

--- utils/main.py
+++ utils/main.py
@@ -3,13 +3,18 @@ from functions import json_load
 from functions import date_sorter
 from functions import number_formatter
 
+# Создаем список из json-файла
 first_list = json_load("../operations.json")
 
+# Удаляем из списка пустые элементы
 for item in first_list.copy():
     if item == {}:
         first_list.remove(item)
 
+# Создаем список, сортированный по датам операций
 second_list = date_sorter(first_list)
+
+# Создаем список из последних 5 выполненных операций
 third_list = []
 counter = 0
 for item_ in second_list:
@@ -17,9 +22,10 @@ for item_ in second_list:
         third_list.append(item_)
         counter += 1
 
+# Выводим эти операции в удобном для пользователя виде
 for item__ in third_list:
     thedate = date.fromisoformat(item__["date"][0:10])
-    date_formated = thedate.strftime("%d.%m.%y")
+    date_formated = thedate.strftime("%d.%m.%Y")
     description = item__["description"]
     amount = item__["operationAmount"]["amount"]
     currency_type = item__["operationAmount"]["currency"]["name"]
