e7a1558a46909eda2b7222a1858dabdd302c1a67
DanilChebansky <151018836+DanilChebansky@users.noreply.github.com>
2024-03-31 03:37:03 +0300

    Added docstrings in functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -2,16 +2,19 @@ import json
 
 
 def json_load(json_file):
+    """Открывает json-файл"""
     with open(json_file) as file:
         return json.load(file)
 
 
 def date_sorter(list_):
+    """Сортирует список по дате, начиная с ближайшей"""
     sorted_list = sorted(list_, key=lambda x: x["date"], reverse=True)
     return sorted_list
 
 
 def number_formatter(str_):
+    """Редактирует числовые значения номера карты или счета"""
     if len(str_) == 20:
         new_str = f"**{str_[-4:]}"
         return new_str
